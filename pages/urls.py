abb5e85013fa8da61ab3f7bb838915df8c42afc2
courtney-swift <courtneydezman@gmail.com>
2026-08-14 12:15:25 +0900

    Contact & About Updated

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView
+from .views import HomePageView, AboutPageView, contact_page 
 
 
 urlpatterns = [
   path('', HomePageView.as_view(), name="home"),
   path('about/', AboutPageView.as_view(), name="about"),
+  path('contact/', contact_page, name="contact")
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,18 @@
 from django.views.generic import TemplateView
+from django.shortcuts import HttpResponse, render
 
 # Create your views here.
 # Function based Views vs. Class Based Views
 
 
-class HomePageView(TemplateView):
+class HomePageView(TemplateView): #Heritage - OOP (Object Oriented Programming)
   template_name = "pages/home.html"
 
 class AboutPageView(TemplateView):
   template_name = "pages/about.html"
+
+
+# Function Based Views
+def contact_page(request):
+  # return HttpResponse("Hello World from FBV")
+  return render(request, "pages/contact.html")
